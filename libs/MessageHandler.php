7501067c1ccab4e9febcffcda3399ede4102496c
zxz <zxz@ayohei.com>
2018-12-06 17:17:50 +0800

    子进程结束后僵尸进程清除处理

--- libs/MessageHandler.php
+++ libs/MessageHandler.php
@@ -13,6 +13,10 @@ class MessageHandler
      */
     static public function msgDeal(&$connector, &$frame, $isDoFork = true)
     {
+        declare(ticks = 1);
+        //清除子进程结束后的僵尸进程的生成，pcntl_signal(SIGCHLD, SIG_IGN)通知内核，自己对子进程的结束不感兴趣，那么子进程结束后，内核会回收，并不再给父进程发送信号
+        pcntl_signal(SIGCHLD, SIG_IGN);
+
         if ($isDoFork) {
             $pid = pcntl_fork();
             if ($pid == -1) {
@@ -21,14 +25,10 @@ class MessageHandler
                 echo '当前内存使用量：' . memory_get_usage(true) . PHP_EOL;
                 echo '当前子进程pid：' . posix_getpid() . PHP_EOL;
 
-                try{
-                    self::distributor($connector, $frame);
-                }catch (\Exception $exception){
-                    echo "信息分发错误，错误信息：" . $exception->getMessage() . PHP_EOL;
-                }
+                self::distributor($connector, $frame);
 
-                //处理完信息之后杀死进程
-                posix_kill(posix_getpid(), SIGTERM);
+                //子进程结束
+                $connector->server->stop();
             } else {
                 //echo "I'm the parent process 子进程的pid值：{$pid} \n";
             }
@@ -49,23 +49,27 @@ class MessageHandler
      */
     static function distributor(&$connector, &$frame)
     {
-        echo str_repeat('=', 20) . PHP_EOL;
+        try {
+            echo str_repeat('=', 20) . PHP_EOL;
 
-        //信息处理
-        echo "收到的信息: {$frame->data}" . PHP_EOL;
-        echo "链接id: {$frame->fd}" . PHP_EOL;
+            //信息处理
+            echo "收到的信息: {$frame->data}" . PHP_EOL;
+            echo "链接id: {$frame->fd}" . PHP_EOL;
 
-        $data = self::parseData($frame->data);//传回来的信息是json
+            $data = self::parseData($frame->data);//传回来的信息是json
 
-        //消息分发器构建
-        $messageDistributor = $connector->messageDistributor;
-        /** @var MessageDistributor $distributor */
-        $distributor = new $messageDistributor($connector, $frame, $data);
-        $distributor->run();
+            //消息分发器构建
+            $messageDistributor = $connector->messageDistributor;
+            /** @var MessageDistributor $distributor */
+            $distributor = new $messageDistributor($connector, $frame, $data);
+            $distributor->run();
 
-        echo str_repeat('=', 20) . PHP_EOL;
+            echo str_repeat('=', 20) . PHP_EOL;
 
-        //发送信息后事件处理
-        $connector->triggerEvent('afterMessage', [&$connector->server, &$frame]);
+            //发送信息后事件处理
+            $connector->triggerEvent('afterMessage', [&$connector->server, &$frame]);
+        } catch (\Exception $exception) {
+            echo "信息分发错误，错误信息：" . $exception->getMessage() . PHP_EOL;
+        }
     }
 }
